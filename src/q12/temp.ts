f1eb33ed5b79f78d8c017031d0f8afe37105a73a
Nithima44 <nitima7859@gmail.com>
2025-12-15 17:46:12 +0700

    temp.ts

---
+++ src/q12/temp.ts
@@ -0,0 +1,10 @@
+const temp = Number(process.argv[2]);
+if (isNaN(temp)){
+    console.log("Please enter a valid number.");
+    process.exit(1);
+}
+if (temp > 30){
+    console.log("Hot.");
+} else {
+    console.log("Normal.");
+}
