125dbffdd63cc3cf7bdae21531e64c0db5ec1e27
Nithima44 <nitima7859@gmail.com>
2025-12-15 18:26:13 +0700

    Even.ts

--- src/q14/Even.ts
+++ src/q14/Even.ts
@@ -1,9 +1,11 @@
 const n = Number(process.argv[2]);
-if (isNaN(n) || ) {
+if (isNaN(n)) {
     process.exit(0);
 }
 for (let i = 0; i <= n; i += 2) {
     if (i%2 === 0) {
         console.log(i.toString());
+    } else {
+        continue;
     }
 }
