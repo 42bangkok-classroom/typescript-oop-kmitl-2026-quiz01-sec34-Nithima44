4403a1ca01f06a162b38e10449bee465b8f594d0
Nithima44 <nitima7859@gmail.com>
2025-12-15 18:11:00 +0700

    LargestNumber.ts

--- src/q20/LargestNumber.ts
+++ src/q20/LargestNumber.ts
@@ -1,12 +1,12 @@
-const args = process.argv.slice(2);
-if (args.length === 0) {
+const arg = process.argv.slice(2);
+if (arg.length === 0) {
     console.log("Please provide at least one number.");
     process.exit(1);
 }
-let largest = Number(args[0]);
+let largest = Number(arg[0]);
 
-for (let i = 1; i < args.length; i++) {
-    const num = Number(args[i]);
+for (let i = 1; i < arg.length; i++) {
+    const num = Number(arg[i]);
     if (!isNaN(num) && num > largest) {
         largest = num;
     }
